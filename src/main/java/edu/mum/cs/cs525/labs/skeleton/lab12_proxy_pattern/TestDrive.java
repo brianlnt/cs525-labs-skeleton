6aedc9d25c73a4b96fe66a5d7e4d6591a031404c
Brian <thinhle1199@gmail.com>
2024-09-18 17:29:44 -0500

    create Dynamic Proxy

--- src/main/java/edu/mum/cs/cs525/labs/skeleton/lab12_proxy_pattern/TestDrive.java
+++ src/main/java/edu/mum/cs/cs525/labs/skeleton/lab12_proxy_pattern/TestDrive.java
@@ -1,9 +1,11 @@
 package edu.mum.cs.cs525.labs.skeleton.lab12_proxy_pattern;
 
+import java.lang.reflect.Proxy;
+
 public class TestDrive {
     public static void main(String[] args) {
         // Create the virtual proxy
-        ComplexInterface proxy = VirtualProxyHandler.createProxy();
+        ComplexInterface proxy = createProxy();
 
         System.out.println("Proxy created. The real object is not created yet.");
 
@@ -12,6 +14,14 @@ public class TestDrive {
 
         // The real object is already created, so this time it won’t take 10 seconds
         proxy.veryComplicatedTask();
+    }
+
+    public static ComplexInterface createProxy() {
+        return (ComplexInterface) Proxy.newProxyInstance(
+            ComplexInterface.class.getClassLoader(),
+            new Class[] { ComplexInterface.class },
+            new VirtualProxyHandler()
+        );
     }
 }
 
--- src/main/java/edu/mum/cs/cs525/labs/skeleton/lab12_proxy_pattern/VirtualProxyHandler.java
+++ src/main/java/edu/mum/cs/cs525/labs/skeleton/lab12_proxy_pattern/VirtualProxyHandler.java
@@ -2,7 +2,6 @@ package edu.mum.cs.cs525.labs.skeleton.lab12_proxy_pattern;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class VirtualProxyHandler implements InvocationHandler {
     private ComplexInterface realObject = null; // Real object is not created initially
@@ -14,13 +13,5 @@ public class VirtualProxyHandler implements InvocationHandler {
             realObject = new ComplexClass();  // Lazy initialization of the real object
         }
         return method.invoke(realObject, args);  // Forward method call to the real object
-    }
-
-    public static ComplexInterface createProxy() {
-        return (ComplexInterface) Proxy.newProxyInstance(
-            ComplexInterface.class.getClassLoader(),
-            new Class[] { ComplexInterface.class },
-            new VirtualProxyHandler()
-        );
     }
 }
